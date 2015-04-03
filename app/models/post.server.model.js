9e96c10152169e0ebed93c5ebf146f33e3230fee
Marc Cruz <fcpmarcus@gmail.com>
2015-04-03 08:58:29 -0700

    recommit

---
+++ app/models/post.server.model.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Post Schema
+ */
+var PostSchema = new Schema({
+	title: {
+		type: String,
+		default: '',
+		required: 'Please fill Post title',
+		trim: true
+	},
+	// calling the body post_body because just body is a pretty common string
+  post_body: {
+      type: String,
+      default: '',
+      required: 'Please fill Post body',
+      trim: true
+  },
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('Post', PostSchema);
